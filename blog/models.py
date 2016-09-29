3a02a95bf33e52dc1e31340c0f2d6c9fdd8c5bbd
Anartz Muxika Ledo <mugan86@gmail.com>
2016-09-29 13:06:59 +0200

    Create 'blog' application inside 'camperp' project and define 'Post' model in moelds.py file

---
+++ blog/models.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User') #User of django
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+                        default=timezone.now)
+    published_date = models.DateTimeField(
+                        blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    ## In python 2 to show in select options for example use 'def __unicode__' and in python 3 use 'def __str__'
+    def __unicode__(self):
+        return u'%s' % self.title
