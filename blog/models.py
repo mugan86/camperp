ecccd623c847f7cb41dd2816f90e852836771fc5
anartzmugika <mugan86@gmail.com>
2016-10-01 09:00:26 +0200

    Add id property with primary_key

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
+    id = models.AutoField(primary_key=True)
     author = models.ForeignKey('auth.User') #User of django
     title = models.CharField(max_length=200)
     text = models.TextField()
