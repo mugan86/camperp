d5bb9f75a293cca1e2ec78db55d6fab973fc20be
Anartz Muxika Ledo <mugan86@gmail.com>
2016-10-04 09:53:14 +0200

    Create PostType model

--- blog/models.py
+++ blog/models.py
@@ -5,11 +5,29 @@ from django.db import models
 from django.utils import timezone
 
 
+class PostType(models.Model):
+    id = models.AutoField(primary_key=True)
+    type = models.CharField(default="", max_length=50)
+    description = models.CharField(default="", max_length=200, blank=True)
+    created_date = models.DateField(default=timezone.now)
+
+    def publish(self):
+        self.save()
+
+    ## In python 2 to show in select options for example use 'def __unicode__' and in python 3 use 'def __str__'
+    def __unicode__(self):
+        return u'%s - %s' % (self.type, self.description)
+
+    class Meta:
+        unique_together = (("type",),)
+
+
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
     author = models.ForeignKey('auth.User')  # User of django
     title = models.CharField(max_length=200)
     text = models.TextField()
+    type = models.ForeignKey(PostType)
     created_date = models.DateTimeField(
         default=timezone.now)
     published_date = models.DateTimeField(
