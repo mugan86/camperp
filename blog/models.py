a2c257b1d95b3d47fa41071495575f8bf02f033b
Anartz Muxika Ledo <mugan86@gmail.com>
2016-10-04 12:19:02 +0200

    Add PostCategory model and relational in Post Model

--- blog/models.py
+++ blog/models.py
@@ -21,6 +21,22 @@ class PostType(models.Model):
     class Meta:
         unique_together = (("type",),)
 
+class PostCategory(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(default="", max_length=50)
+    description = models.CharField(default="", max_length=200, blank=True)
+    created_date = models.DateTimeField(default = timezone.now)
+
+    def publish(self):
+        self.save()
+
+    ## In python 2 to show in select options for example use 'def __unicode__' and in python 3 use 'def __str__'
+    def __unicode__(self):
+        return u'%s - %s' % (self.name, self.description)
+
+    class Meta:
+        unique_together = (("name",),)
+
 
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
@@ -28,6 +44,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     type = models.ForeignKey(PostType)
+    category = models.ForeignKey(PostCategory)
     created_date = models.DateTimeField(
         default=timezone.now)
     published_date = models.DateTimeField(
