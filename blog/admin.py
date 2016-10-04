36fa21cba9682ad4b03838529acaca55eda9d8a8
Anartz Muxika Ledo <mugan86@gmail.com>
2016-10-04 12:19:22 +0200

    Register PostCategory model in admin to manage info of post categories

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post, PostType
+from .models import Post, PostType, PostCategory
 
 admin.site.register(Post)
 admin.site.register(PostType)
+admin.site.register(PostCategory)
