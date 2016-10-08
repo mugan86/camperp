e6df91e33408db96e475dd96f7488e01ade8b515
anartzmugika <mugan86@gmail.com>
2016-10-08 21:21:43 +0200

    Update settings.py

--- camperp/settings.py
+++ camperp/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-
 TINYMCE_DEFAULT_CONFIG = {
+    'plugins': "table,spellchecker,paste,searchreplace",
     'theme': "advanced",
 }
