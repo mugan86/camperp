9187623c0166a99f518551bf90d29595dd4a061a
anartzmugika <mugan86@gmail.com>
2016-10-08 10:12:16 +0200

    Add tinymce editor app in installed apps and configure

--- camperp/settings.py
+++ camperp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+TINYMCE_DEFAULT_CONFIG = {
+    'theme': "advanced",
+}
