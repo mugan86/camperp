2b3b9164a9ceec087cd89842f65e7705713d1f1c
anartzmugika <mugan86@gmail.com>
2016-10-14 20:51:07 +0200

    Add update_date field

--- blog/forms.py
+++ blog/forms.py
@@ -5,4 +5,4 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'intro_text', 'text', 'type', 'category',)
+        fields = ('title', 'intro_text', 'text', 'type', 'category', 'update_date',)
