516f53dc5c8b18ea6357d8f562d8bf4699a55df0
anartzmugika <mugan86@gmail.com>
2016-10-20 20:35:04 +0200

    Create advanced admin model

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Post, PostType, PostCategory
 
+
+class PostTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type', 'description', 'created_date', )
+
 admin.site.register(Post)
-admin.site.register(PostType)
+admin.site.register(PostType, PostTypeAdmin)
 admin.site.register(PostCategory)
